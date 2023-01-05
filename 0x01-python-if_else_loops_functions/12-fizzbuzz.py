8ddc4647c764274be78d2adec57442542da32ae3
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-01-05 18:48:26 +0000

    FizzBuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     for num in range(1, 101):
         if num % 3 == 0 and num % 5 != 0:
-            print('fizz', end=' ')
+            print('Fizz', end=' ')
         elif num % 5 == 0 and num % 3 != 0:
             print('Buzz', end=' ')
         elif num % 3 == 0 and num % 5 == 0:
