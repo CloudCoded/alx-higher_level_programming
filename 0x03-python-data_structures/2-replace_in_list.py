d47a76f3a27800425993bce5e713aa0b5bb5ffca
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-01-09 13:31:40 +0000

    python correction

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def replace_in_list(my_list, element):
+def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_last):
         return my_list
     my_list[idx] = element
