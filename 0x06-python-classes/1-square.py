7bf0217f9e2afa0a29e676cca8490015afaf0788
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-01-24 13:55:13 +0000

    python program

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Square class definition"""
+
+
+class Square:
+    """Represents a square
+    Attributes:
+       __size (init): size of a side of the square
+    """
+    def __init__(self, size):
+        """Initializes a square
+        Args:
+           size (init): size of a side of the square
+        Returns: None
+        """
+        self.__size = size
